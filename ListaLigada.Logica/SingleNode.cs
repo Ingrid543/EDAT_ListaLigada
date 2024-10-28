27f9490f4f4625c1b9155dbf320fad2da4b88815
Ingrid543 <23030396@itesa.edu.mx>
2024-10-28 08:39:23 -0600

    Version_02:Metodo Agregar elemento y ToString.

---
+++ ListaLigada.Logica/SingleNode.cs
@@ -0,0 +1,14 @@
+﻿namespace ListaLigada.Logica
+{
+    public class SingleNode <T>
+    {
+        public T? Data { get; set; } //guarda varibales de diferentes tipos
+
+        public SingleNode<T>? Next { get; set; }//dirección de memoria(puntero, guarda direcciones de memoria)
+
+        public SingleNode(T data) { 
+            Data = data;
+
+        }
+    }
+}
--- ListaLigada.UI.CONSOLA/Program.cs
+++ ListaLigada.UI.CONSOLA/Program.cs
@@ -1,8 +1,18 @@
-﻿internal class Program
+﻿using ListaLigada.Logica;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Lista Ligada Simple");
+        var singleList = new SingleList<string>();
+
+        singleList.ADD("Alondra");
+        singleList.ADD("Karla");
+        singleList.ADD("Juan");
+        singleList.ADD("Luis");
+        singleList.ADD("Jose");
+        Console.WriteLine(singleList);
 
         Console.ReadKey();
     }
